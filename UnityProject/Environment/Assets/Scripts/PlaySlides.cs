94e76856c7ddd5d18089036efabf21a195c9ab30
Ben <benjaminmoder@gmail.com>
2021-02-15 05:50:57 -0600

    Upgrades project to Universal Render Pipeline

---
+++ UnityProject/Environment/Assets/Scripts/PlaySlides.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySlides : MonoBehaviour
+{
+	
+	SpriteRenderer spriteRenderer;
+	
+	public List<SlideOrder> slides = new List<SlideOrder>();
+	
+	void Start(){
+		spriteRenderer = this.GetComponent<SpriteRenderer>();
+		
+		StartCoroutine(Sequence());
+	}
+	
+    void ChangeSprite(int x){
+		spriteRenderer.sprite = slides[x].slide;
+	}
+	
+	IEnumerator Sequence(){
+		
+		for (int curSlide = 0; curSlide < slides.Count; curSlide++){
+			
+			ChangeSprite(curSlide);
+			
+			yield return new WaitForSeconds(slides[curSlide].holdTime);
+			
+		}
+	}
+}
